6287798652366205e36b6142a09129546d6683ba
Kaushal Kawade <kaushal.kawade@oracle.com>
2023-11-01 15:31:34 +0530

    all the explaination of angular files,and components written.

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/Todo';
+// here @Comonent is ts file of component each component contain 4 files html,css,ts,spec.ts
+// in html and css ui code is there
+// in ts behevior of code we write
+// This is ts file there are variables like
+//1) selector - it is name of component => how we use this component in our app
+//   ex here <app-todos></app-todos> like that we are using this component
+//2) templateUrl is url of html file all html code of component is here ||| or we cant write direct HTML code there also
+//3) styleUrls here all cass code of component is there ||| we can also write direct css code here.
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
